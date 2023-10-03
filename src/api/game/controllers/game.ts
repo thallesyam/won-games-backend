9ec30fac99a0e11a2f666403e826f234d619d976
Thalles Silva <thallesyam@DESKTOP-S2FIV5R.localdomain>
2023-10-03 20:45:44 -0300

    feat: create webscrapping to fill data

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -4,4 +4,18 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::game.game');
+export default factories.createCoreController('api::game.game', ({ strapi }) => ({
+  async populate(ctx) {
+    const options = {
+      limit: 48,
+      order: 'desc:trending',
+      ...ctx.query
+    }
+    console.log('Starting populate...');
+
+    await strapi.service("api::game.game").populate(options)
+    
+    ctx.send('Finished populate...')
+  }
+
+}));
